a357ca9161c09161862d323698073e38208b31b7
Lucas Vidal <lucasvidal0904@gmail.com>
2022-09-04 20:40:04 -0300

    Crear carpetas y archivos

---
+++ bakend/sql/ConsultarTablas_TPV.sql
@@ -0,0 +1,18 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+SELECT TOP (1000) *
+  FROM [Terminal Punto Venta].[dbo].[Stock]
+
+  SELECT TOP (1000) *
+  FROM [Terminal Punto Venta].[dbo].[Cliente]
+
+  SELECT TOP (1000) *
+  FROM [Terminal Punto Venta].[dbo].[ID_Ventas]
+
+  SELECT TOP (1000) *
+  FROM [Terminal Punto Venta].[dbo].[Ventas]
+
+  SELECT TOP (1000) *
+  FROM [Terminal Punto Venta].[dbo].[ID_Compras]
+
+  SELECT TOP (1000) *
+  FROM [Terminal Punto Venta].[dbo].[Compras]
