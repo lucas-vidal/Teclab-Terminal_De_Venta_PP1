5c57f777a29fef1c34a140ba0a387a08c63529b6
Lucas Vidal <lucasvidal0904@gmail.com>
2022-09-20 00:45:13 -0300

    modif main.js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,36 +1,76 @@
 
 
+let codigo;
+let codigo2;
+let cantidad;
+let total = 0;
+let item = 0;
 
-let codigo1 = parseInt(document.getElementById('codigo').value, 10);
-let cantidad1 = parseInt(document.getElementById('cantidad').value, 10);
-
-var item = 0;
-var codigo;
 var descripcion = "Fideito Moñito";
-var cantidad;
-var precio = 150;
+
+var precio = 100;
 
 function agregarItem(codigo, cantidad)
 {
+codigo = parseInt((document.getElementById('codigo')).value, 10);
+cantidad = parseInt((document.getElementById('cantidad')).value, 10);
 
-const app = document.querySelector("#registro");
+if (document.getElementById('codigo').value != "" && document.getElementById('cantidad') != "" ) {
+    const app = document.querySelector("#registro");
+    total = total + precio;
+    item = item + 1;
+
+    var deshacer = '<button onclick="eliminarItem(\'#registro' + item + '\')" class="mr-2 btn-icon btn-icon-only btn btn-outline-danger"><i class="pe-7s-trash btn-icon-wrapper"> </i></button>';
+ 
+    app.insertAdjacentHTML("afterbegin", '<tr id="registro' + item + '"><td class="text-center">'+ item +'</td><td class="text-center">'+ codigo +'</td><td class="text-left">'+ descripcion +'</td><td class="text-center">'+ cantidad +'</td><td class="text-center">'+ precio +'</td><td class="text-center">' + deshacer + '</td></tr>');
+
+    limpiarInput('codigo');
+    limpiarInput('cantidad')
+
+    document.getElementById("total").innerHTML = "$ " + total ;
+
+} else {
+    
+    window.alert("Complete los campos.");
+}
 
 
 
-item = item + 1;
+};
 
-var deshacer = '<button onclick="eliminarItem("#registro3")" class="mr-2 btn-icon btn-icon-only btn btn-outline-danger"><i class="pe-7s-trash btn-icon-wrapper"> </i></button>';
+document.getElementById('codigo').addEventListener("change","three")
 
-   return app.insertAdjacentHTML("afterbegin", '<tr id="registro' + item + '"><td class="text-center">'+ item +'</td><td class="text-center">'+ codigo +'</td><td class="text-left">'+ descripcion +'</td><td class="text-center">'+ cantidad +'</td><td class="text-center">'+ precio +'</td><td class="text-center">' + deshacer + '</td></tr>');
-  
+
+if (codigo2 =! document.getElementById('codigo').value) {
+    buscarProducto();
+    
+};
+
+
+
+
+
+function buscarProducto(){
+    window.alert("Se ejecuto ok.");
+    codigo2 = document.getElementById('codigo').value;
+};
+
+
+function limpiarInput(id){
+    document.getElementById(id).value = "";
+
 };
 
 function eliminarItem(id){
     
+    var app = document.querySelector(id);
 
-    const app = document.querySelector(id);
-
-    app.isConnected;    // true
+    app.isConnected;
     app.remove();
-    app.isConnected;    // false
+    app.isConnected;
+
+    total = total - precio;
+
+    document.getElementById("total").innerHTML = "$ " + total ;
 };
+
