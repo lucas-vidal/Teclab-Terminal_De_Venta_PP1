b635fe980f45910939afe0b7da337c971ef4851d
Lucas Vidal <lucasvidal0904@gmail.com>
2022-09-22 00:14:11 -0300

    test de conexion a base de datos

---
+++ bakend/cnx.js
@@ -0,0 +1,14 @@
+   
+    const cnx = {
+    user: 'lucas_test',
+    password: 'lucas1234',
+    server: 'LUCAS\\SQLEXPRESS',
+    database: 'Terminal_PDV',
+    options:{
+        trustedconnection : false,
+        enableArithAbort : true,
+        encrypt : false,
+    }
+}
+module.exports = cnx;
+
--- bakend/getEs.js
+++ bakend/getEs.js
@@ -1,17 +1,16 @@
-const cnx = require('/conexion');
+const cnx = require('./cnx');
 const sql = require('mssql');
 
-async function getDatos(){
+async function getEst(){
     try{
         let pool = await sql.connect(cnx);
-        let salida = await pool.request().query("select * from stock")
+        let salida = await pool.request().query("select * from Stock");
     }
     catch (err){
         console.log(err);
     }
-};
-
-getDatos();
+}
+getEst();
 module.exports = {
-    getDatos: getDatos
+    getEs: getEst
 }
